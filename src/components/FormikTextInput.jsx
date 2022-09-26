1031697579157ba84d9fb11a3407c095deb241da
jyk <jkartila@hotmail.com>
2022-09-26 17:19:20 +0300

    errror

--- src/components/FormikTextInput.jsx
+++ src/components/FormikTextInput.jsx
@@ -8,10 +8,9 @@ const styles = StyleSheet.create({
   errorText: {
     marginTop: 5,
     fontSize: 15,
-    color: 'red'
-
+    color: 'red',
+    borderColor: 'red',
   }
-
   , text : {
    color: 'black',
    backgroundColor: 'white', 
@@ -19,23 +18,22 @@ const styles = StyleSheet.create({
    hight: 250,
    borderRadius: 4,
    textAlign: 'left',
-   borderWidth: 1,
-   borderColor: "gray"
+   borderWidth: 2, // laita 3 kun error
+   borderColor: "red" // laita red kun error
 
 
 
   }
-  
 });
 
 const FormikTextInput = ({ name, ...props }) => {
   const [field, meta, helpers] = useField(name);
   const showError = meta.touched && meta.error;
 
-
+ // style={{error: true ? styles.text:styles.errorText}}
   return (
     <>
-      <TextInput style={styles.text}
+      <TextInput style={meta.error ? styles.text:styles.errorText}
         onChangeText={value => helpers.setValue(value)}
         onBlur={() => helpers.setTouched(true)}
         value={field.value}
@@ -43,6 +41,7 @@ const FormikTextInput = ({ name, ...props }) => {
         {...props}
       />
       {showError && <Text style={styles.errorText}>{meta.error}</Text>}
+      
     </>
   );
 };
